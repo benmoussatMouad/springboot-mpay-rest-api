fb2ce59d3256dd2a2edacea3038900f0bed633ed
benmoussatMouad <hm_benmoussat@esi.dz>
2023-05-10 23:30:10 +0100

    First Commit: Project setup - Agency CRUDS - Bank CRUDS - User simple Auth - Client Signup and login

--- src/main/java/com/springboot/mpaybackend/repository/WilayaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.mpaybackend.repository;
-
-import com.springboot.mpaybackend.entity.Wilaya;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WilayaRepository extends JpaRepository<Wilaya, Long> {
-}
