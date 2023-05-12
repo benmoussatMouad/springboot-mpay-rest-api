d80332e441a41d4ed7c53e3f5bae29d923409c62
benmoussatMouad <hm_benmoussat@esi.dz>
2023-05-12 18:23:50 +0100

    remove Category, Comment and POST CRUDS

--- src/main/java/com/springboot/mpaybackend/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.mpaybackend.repository;
-
-import com.springboot.mpaybackend.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
