e2f8eddcf5b38497c54733da656688f8eea325d5
benmoussatMouad <hm_benmoussat@esi.dz>
2023-06-18 18:09:43 +0100

    Fix scanned files dto must include files and should not be empty

--- src/main/java/com/springboot/mpaybackend/controller/ScannedFilesRequestDto.java
+++ src/main/java/com/springboot/mpaybackend/controller/ScannedFilesRequestDto.java
@@ -2,12 +2,16 @@ package com.springboot.mpaybackend.controller;
 
 import com.springboot.mpaybackend.payload.MerchantFileDto;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.NonNull;
 
 import java.util.List;
 
 @Data
 public class ScannedFilesRequestDto {
-    @Schema(name = "Scanned merchant documents", description = "Must be five objects, the merchantId field can be omitted.")
+    @Schema(description = "Must be five objects, the merchantId field can be omitted.")
+    @NotNull
     private List<MerchantFileDto> files;
+
 }
