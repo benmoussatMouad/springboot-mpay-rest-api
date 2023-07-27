8ac709f6dd70a1fa6fa2f5e80e1fd41276223cc3
benmoussatMouad <hm_benmoussat@esi.dz>
2023-07-27 18:37:08 +0100

    Fix client username is null

--- src/main/java/com/springboot/mpaybackend/payload/MerchantByBankUserDto.java
+++ src/main/java/com/springboot/mpaybackend/payload/MerchantByBankUserDto.java
@@ -1,6 +1,7 @@
 package com.springboot.mpaybackend.payload;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
