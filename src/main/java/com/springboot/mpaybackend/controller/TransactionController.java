0871345bceb58ecb88f51ba4471b664de6323d20
benmoussatMouad <hm_benmoussat@esi.dz>
2023-08-24 18:36:44 +0100

    Fix init-payment always returning an exception

--- src/main/java/com/springboot/mpaybackend/controller/TransactionController.java
+++ src/main/java/com/springboot/mpaybackend/controller/TransactionController.java
@@ -44,7 +44,7 @@ public class TransactionController {
     @PostMapping("init-payment")
     @PreAuthorize("hasAnyAuthority('CLIENT')")
     public ResponseEntity<String> initiatePayment(@RequestBody CardDataDto dto) {
-        if (transactionService.confirmCardData(dto.getPan(), dto.getCvv(), dto.getMonth(), dto.getYear(), dto.getName())) {
+        if (!transactionService.confirmCardData(dto.getPan(), dto.getCvv(), dto.getMonth(), dto.getYear(), dto.getName())) {
             throw new MPayAPIException(HttpStatus.FORBIDDEN, "Card data are wrong");
         }
         otpService.createOtp(dto.getUsername());
@@ -69,7 +69,7 @@ public class TransactionController {
     @PostMapping("authenticate-payment")
     @PreAuthorize("hasAnyAuthority('CLIENT')")
     public ResponseEntity<String> authenticateSatimPayement() {
-        return null;
+        return ResponseEntity.ok( "Card authenticated" );
     }
 
     @PutMapping("/{id}/authenticated")
