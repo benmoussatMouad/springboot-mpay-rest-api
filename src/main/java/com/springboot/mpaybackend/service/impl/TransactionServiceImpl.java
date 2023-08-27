fb9258c5c843056416875d2a183a95a098c6c4ba
benmoussatMouad <hm_benmoussat@esi.dz>
2023-08-27 19:15:11 +0100

    refactor generation of order Id: create a string of random characters

--- src/main/java/com/springboot/mpaybackend/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/springboot/mpaybackend/service/impl/TransactionServiceImpl.java
@@ -19,6 +19,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 @Service
@@ -51,8 +52,25 @@ public class TransactionServiceImpl implements TransactionService {
         dto.setAmount(amount);
         dto.setMerchantId(merchant.getId());
         dto.setMerchantName(merchant.getLastName() + " " + merchant.getFirstName());
-        dto.setOrderId( Double.valueOf(Math.random()*100000).toString());
+        dto.setOrderId(generaetRandomString() );
         return dto;
+    }
+
+    private String generaetRandomString() {
+ 
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int) 
+              (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        String generatedString = buffer.toString();
+    
+        return generatedString;
     }
 
     @Override
