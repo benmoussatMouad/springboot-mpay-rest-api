99fcbfdfe44693984f561a76fb0726e180709af5
benmoussatMouad <hm_benmoussat@esi.dz>
2023-08-27 16:21:08 +0100

    Fix error in SQL query syntax

--- src/main/java/com/springboot/mpaybackend/repository/TransactionRepository.java
+++ src/main/java/com/springboot/mpaybackend/repository/TransactionRepository.java
@@ -20,7 +20,7 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
         "AND (:type is null OR t.type = :type) " + // Added space after t.type
         "AND (:pan is null OR CONCAT('%', cc.cardFirst6Numbers, '%') LIKE CONCAT('%', :pan, '%') OR CONCAT('%', cc.cardLast4Numbers, '%') LIKE CONCAT('%', :pan, '%')) " +
         "AND (:pan is not null OR :last4 is null OR :last4 = cc.cardLast4Numbers) " +
-        "AND (:startDate is null OR :endDate is null OR (t.transactionDate >= :startDate AND t.transactionDate <= :endDate ))) " +
+        "AND (:startDate is null OR :endDate is null OR (t.transactionDate >= :startDate AND t.transactionDate <= :endDate )) " +
         "AND (t.deleted = FALSE)")
     Page<Transaction> findByFilter(Pageable pageable, Long id, String orderId, String terminalId, String phone, String status, String startDate, String endDate, String type, String pan, String last4);
 
@@ -33,12 +33,12 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
         "AND (:type is null OR t.type = :type) " + // Added space after t.type
         "AND (:pan is null OR CONCAT('%', cc.cardFirst6Numbers, '%') LIKE CONCAT('%', :pan, '%') OR CONCAT('%', cc.cardLast4Numbers, '%') LIKE CONCAT('%', :pan, '%')) " +
         "AND (:pan is not null OR :last4 is null OR :last4 = cc.cardLast4Numbers) " +
-        "AND (:startDate is null OR :endDate is null OR (t.transactionDate >= :startDate AND t.transactionDate <= :endDate ))) " +
+        "AND (:startDate is null OR :endDate is null OR (t.transactionDate >= :startDate AND t.transactionDate <= :endDate )) " +
         "AND (t.deleted = FALSE)")
     Page<Transaction> findByFilterAndMerchant(Pageable pageable, Long id, String orderId, String terminalId, String phone, String status, String startDate, String endDate, String username, String type, String pan, String last4);
 
     @Query("SELECT t FROM Transaction t, Client c, Merchant m, ClientCard cc WHERE (:id is null or t.id = :id) " +
-        "AND (:username is null OR c.username = :username) " +
+        "AND (:username is null OR c.user.username = :username) " +
         "AND (t.merchant.id = m.id) AND (t.client is null OR t.client.id = c.id) AND (t.client is null OR cc.client.id = c.id) " + // Added space after c.id
         "AND ((:orderId is null OR CONCAT('%', t.orderId, '%') LIKE CONCAT('%', :orderId, '%')) AND (:terminalId is null OR CONCAT('%', t.terminalId, '%') LIKE CONCAT('%', :terminalId, '%'))) " +
         "AND (:phone is null OR c.phone LIKE CONCAT('%', :phone, '%') OR m.phone LIKE CONCAT('%', :phone, '%') ) " +
@@ -46,7 +46,7 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
         "AND (:type is null OR t.type = :type) " + // Added space after t.type
         "AND (:pan is null OR CONCAT('%', cc.cardFirst6Numbers, '%') LIKE CONCAT('%', :pan, '%') OR CONCAT('%', cc.cardLast4Numbers, '%') LIKE CONCAT('%', :pan, '%')) " +
         "AND (:pan is not null OR :last4 is null OR :last4 = cc.cardLast4Numbers) " +
-        "AND (:startDate is null OR :endDate is null OR (t.transactionDate >= :startDate AND t.transactionDate <= :endDate ))) " +
+        "AND (:startDate is null OR :endDate is null OR (t.transactionDate >= :startDate AND t.transactionDate <= :endDate )) " +
         "AND (t.deleted = FALSE)")
     Page<Transaction> findByFilterAndClient(Pageable pageable, Long id, String orderId, String terminalId, String phone, String status, String startDate, String endDate, String username, String type, String pan, String last4);
 }
